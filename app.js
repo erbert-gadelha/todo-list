779a58a8f8396d143e36ec1065f1d2a741c92e00
ebgr <ebgr@cin.ufpe.br>
2024-07-22 18:39:24 -0300

    PATCH: adicionando model-views

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require('ejs');
 
-app.get('/', (req, res) => {
-  res.send(`<h1>Hello World!</h1>`);
-});
+const express = require('express'),
+      router = require('./routes');
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.set('views','src/views');
+app.set('view engine', 'ejs');
+
+
+const port = 3000;
+app.use(router);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
